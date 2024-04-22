014c342bf4eea91e2553ce297e72f1091c77e86c
Iurii Parkhomenko <i.parkhomenko@astondevs.ru>
2024-04-22 17:07:49 +0300

    - Добавлен логгер в main class

--- src/main/java/ru/parhom/cardatabaseapp/CardatabaseAppApplication.java
+++ src/main/java/ru/parhom/cardatabaseapp/CardatabaseAppApplication.java
@@ -1,10 +1,14 @@
 package ru.parhom.cardatabaseapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardatabaseAppApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(CardatabaseAppApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseAppApplication.class, args);

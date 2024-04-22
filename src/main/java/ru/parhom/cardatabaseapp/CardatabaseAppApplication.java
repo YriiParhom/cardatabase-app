51299780c76d3f01e5bb93528d728413fd95c05a
Iurii Parkhomenko <i.parkhomenko@astondevs.ru>
2024-04-22 17:00:36 +0300

    Initial commit

---
+++ src/main/java/ru/parhom/cardatabaseapp/CardatabaseAppApplication.java
@@ -0,0 +1,13 @@
+package ru.parhom.cardatabaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardatabaseAppApplication.class, args);
+    }
+
+}
